21ad699cf59608224a4dd0a380363d6eb94024c2
ebonwulf <ebonwolf00@gmail.com>
2025-03-29 09:41:52 +0000

    comments added and updated for areas of code not commented sufficiently

--- src/lyssaCorlett/CW1/task3/TaskManager.java
+++ src/lyssaCorlett/CW1/task3/TaskManager.java
@@ -17,88 +17,10 @@ public class TaskManager extends JSplitPane {
                             JTextField taskInputField, JFormattedTextField dateInputField, DateFormat dateFormat,
                             JButton deleteTaskButton, JButton completedTaskButton, JComboBox<String> priorityInputField,
                             JLabel confirmation, JLabel errorLabel, JLabel dateErrorLabel) {
+
+        //creating the task object
         Task task = new Task();
         Object[] options = {"Yes", "Cancel"}; //labels used for the names on JOptionPane buttons
-        //saving task list from text area to a file using the save button
-        saveTaskFile.addActionListener(_ -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int choice = fileChooser.showSaveDialog(null);
-            if(choice == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                String filePath = file.getAbsolutePath();
-                try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-                    int row = 0;
-                    int col = 0;
-                    //collecting data from the task table and storing it in the txt file
-                    while (row < tableModel.getRowCount()) {
-                        rowData.add((String) tableModel.getValueAt(row, col));
-                        rowData.add((String) tableModel.getValueAt(row, col + 1));
-                        rowData.add((String) tableModel.getValueAt(row, col + 2));
-                        rowData.add((String) tableModel.getValueAt(row, col + 3));
-                        row += 1;
-                    }
-                    //removing the [] from around the file data
-                    String taskData = rowData.toString().replace("[", "").replace("]", "");
-                    writer.write(taskData);
-                    JOptionPane.showMessageDialog(inputPanel, "Task list saved");
-                } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(null, "Error saving file");
-                }
-            }
-        });
-
-        //loading a specific file to the text area using the load button
-        loadTaskFile.addActionListener(_ -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int choice = fileChooser.showOpenDialog(table);
-            if(choice == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                    String text = reader.readLine();
-                    String[] taskList = text.split(",");
-                    //loops over the list of data, stored in the taskList array, from the txt file
-                    //and stores each set of data for a task into a new array, then uses this data to populate the table 
-                    for(int i = 0; i < taskList.length; i += 4) {
-                        Object[] data = new Object[4];
-                        data[0] = taskList[i];
-                        data[1] = taskList[i + 1];
-                        data[2] = taskList[i + 2];
-                        data[3] = taskList[i + 3];
-                        tableModel.addRow(data);
-                    }
-                    JOptionPane.showMessageDialog(inputPanel, "Task list loaded");
-                } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(null, "Error loading file");
-                }
-            }
-        });
-
-        //clearing all tasks from task table area
-        clearButton.addActionListener(_ -> {
-            //gives the JOptionPane to check if the user wants to delete the all tasks and only deletes if the "Yes" option is selected
-            int option = JOptionPane.showOptionDialog(inputPanel,
-                    "Are you sure you want to delete all tasks?",
-                    "Delete all tasks",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null, //uses default icon
-                    options, //the names of buttons
-                    options[0]); //default button title
-            if(option == JOptionPane.YES_OPTION) {
-                while(tableModel.getRowCount() > 0) {
-                    for (int i = 0; i < tableModel.getRowCount(); i++) {
-                        rowData.remove((String) tableModel.getValueAt(i, 0));
-                        rowData.remove((String) tableModel.getValueAt(i, 1));
-                        rowData.remove((String) tableModel.getValueAt(i, 2));
-                        rowData.remove((String) tableModel.getValueAt(i, 3));
-                        tableModel.removeRow(i);
-                    }
-                }
-                JOptionPane.showMessageDialog(inputPanel, "All tasks cleared from the list");
-            } else if (option == JOptionPane.NO_OPTION) {
-                JOptionPane.getRootFrame().dispose(); //disposes of the JOptionPane and does not delete the selected task if the no option is selected.
-            }
-        });
 
         //add task to the table area
         addTaskButton.addActionListener(_ -> {
@@ -137,6 +59,87 @@ public class TaskManager extends JSplitPane {
             }
         });
 
+        //saving task list from table in the left panel to a file using the save button
+        saveTaskFile.addActionListener(_ -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int choice = fileChooser.showSaveDialog(null);
+            if(choice == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                String filePath = file.getAbsolutePath();
+                try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                    int row = 0;
+                    int col = 0;
+                    //collecting data from the task table and storing it in the txt file
+                    while (row < tableModel.getRowCount()) {
+                        rowData.add((String) tableModel.getValueAt(row, col));
+                        rowData.add((String) tableModel.getValueAt(row, col + 1));
+                        rowData.add((String) tableModel.getValueAt(row, col + 2));
+                        rowData.add((String) tableModel.getValueAt(row, col + 3));
+                        row += 1;
+                    }
+                    //removing the [] from around the file data
+                    String taskData = rowData.toString().replace("[", "").replace("]", "");
+                    writer.write(taskData);
+                    JOptionPane.showMessageDialog(inputPanel, "Task list saved");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "Error saving file");
+                }
+            }
+        });
+
+        //loading a specific file to the text area using the load button
+        loadTaskFile.addActionListener(_ -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int choice = fileChooser.showOpenDialog(table);
+            if(choice == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String text = reader.readLine();
+                    String[] taskList = text.split(",");
+                    /*loops over the list of data, stored in the taskList array, from the txt file
+                    and stores each set of data for a task into a new array, then uses this data to populate the table*/
+                    for(int i = 0; i < taskList.length; i += 4) {
+                        Object[] data = new Object[4];
+                        data[0] = taskList[i];
+                        data[1] = taskList[i + 1];
+                        data[2] = taskList[i + 2];
+                        data[3] = taskList[i + 3];
+                        tableModel.addRow(data);
+                    }
+                    JOptionPane.showMessageDialog(inputPanel, "Task list loaded");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "Error loading file");
+                }
+            }
+        });
+
+        //clearing all tasks from task table area
+        clearButton.addActionListener(_ -> {
+            //gives the JOptionPane to check if the user wants to delete the all tasks and only deletes if the "Yes" option is selected
+            int option = JOptionPane.showOptionDialog(inputPanel,
+                    "Are you sure you want to delete all tasks?",
+                    "Delete all tasks",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null, //uses default icon
+                    options, //the names of buttons
+                    options[0]); //default button title
+            if(option == JOptionPane.YES_OPTION) {
+                while(tableModel.getRowCount() > 0) {
+                    for (int i = 0; i < tableModel.getRowCount(); i++) {
+                        rowData.remove((String) tableModel.getValueAt(i, 0));
+                        rowData.remove((String) tableModel.getValueAt(i, 1));
+                        rowData.remove((String) tableModel.getValueAt(i, 2));
+                        rowData.remove((String) tableModel.getValueAt(i, 3));
+                        tableModel.removeRow(i);
+                    }
+                }
+                JOptionPane.showMessageDialog(inputPanel, "All tasks cleared from the list");
+            } else if (option == JOptionPane.NO_OPTION) {
+                JOptionPane.getRootFrame().dispose(); //disposes of the JOptionPane and does not delete all the tasks if the cancel option is selected.
+            }
+        });
+
         //delete selected task from the task table
         deleteTaskButton.addActionListener(_ -> {
             //gives the JOptionPane to check if the user wants to delete the task and only deletes if the "Yes" option is selected
@@ -152,7 +155,7 @@ public class TaskManager extends JSplitPane {
                 tableModel.removeRow(table.getSelectedRow());
                 JOptionPane.showMessageDialog(inputPanel, "Task deleted from the list");
             } else if (option == JOptionPane.NO_OPTION) {
-                JOptionPane.getRootFrame().dispose(); //disposes of the JOptionPane and does not delete the selected task if the no option is selected.
+                JOptionPane.getRootFrame().dispose(); //disposes of the JOptionPane and does not delete the selected task if the cancel option is selected.
             }
 
         });
